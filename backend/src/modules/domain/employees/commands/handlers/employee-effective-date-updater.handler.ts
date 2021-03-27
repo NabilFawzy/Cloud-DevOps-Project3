b0ae23f5c5cbdf58b51cadf131794d6eb0aaf3e2
Nabil Fawzy <Nabilfawzy507@gmail.com>
2021-03-27 16:06:38 +0200

    .

--- backend/src/modules/domain/employees/commands/handlers/employee-effective-date-updater.handler.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-effective-date-updater.handler.ts
@@ -19,7 +19,7 @@ export class EmployeeEffectiveDateUpdater extends BaseCommandHandler<UpdateEmplo
 
     const employee = await this.employeeRepository.findById(employeeId);
 
-    employee.effectiveDate = moment(effectiveDate)
+    employee.effectiveDate = new Date(moment(effectiveDate))
     .utc()
     .format();
 
